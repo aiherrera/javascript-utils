54312c825aaf32696a88613a96827287dd684a48
aiherrera <alain.iherrera@gmail.com>
2020-01-29 12:28:54 -0600

    1 file modified

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import './style.css';
  * Returns an array filled with numbers between two points
  * ex. 2-9 -> [2,3,4,5,6,7,8,9]
  */
-export function range(start, end) {
+export function createRange(start, end) {
   return Array(end - start + 1).fill().map((_, idx) => start + idx)
 }
 
@@ -19,26 +19,38 @@ export function checkNumbersOrder(control) {
   let test = control.split('-');
   console.log(test);
 
-  let convertToArray = [];
+  let finalArray = [];
 
   if (test.length > 1) {
-    
-    console.log(test[0].split(',').map(Number))
+    for (const [index, val] of test.entries()) {
+      // console.log(val);
 
-    console.log(
-      range(4, 8)
-    );
+      const convertToInt = val.split(',').map(Number);
+      console.log(convertToInt);
+      finalArray = [...finalArray, ...convertToInt];
+      console.log(convertToInt.pop())
+      finalArray = [...finalArray, ...createRange(convertToInt.pop(), test[index + 1])];
+      finalArray = finalArray.sort((a, b) => a - b);
+
+      console.log(finalArray);
+      
+    }
+    // console.log(test[0].split(',').map(Number).pop())
+
+    // console.log(
+    //   range(4, 8)
+    // );
 
     // console.log(test);
   } else {
-    convertToArray = control.split(/[-,*]+/).sort((a, b) => a - b);
+    finalArray = control.split(/[-,*]+/).sort((a, b) => a - b);
   }
 
-  // console.log(convertToArray);
+  // console.log(finalArray);
 
 }
 
 // Write Javascript code!
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
-checkNumbersOrder('1,2,3,4-8');
+checkNumbersOrder('1,2,3');
